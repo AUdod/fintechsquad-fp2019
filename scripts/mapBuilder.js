bc38304f96f1b694368c95b974c6d12b4aa8f650
Антон Удод <udod@aigeo.ru>
2019-10-08 08:26:09 +0300

    new data

--- scripts/mapBuilder.js
+++ scripts/mapBuilder.js
@@ -12,46 +12,82 @@ var map = new mapboxgl.Map({
 
 });
 
-
-map.on('load', function() {
+map.on('load', function () {
     // Insert the layer beneath any symbol layer.
     var layers = map.getStyle().layers;
 
     console.log(layers);
-     
+
     var labelLayerId;
     for (var i = 0; i < layers.length; i++) {
-    if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-    labelLayerId = layers[i].id;
-    break;
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+        }
     }
-    }
-     
+
     map.addLayer({
-    'id': '3d-buildings',
-    'source': 'composite',
-    'source-layer': 'building',
-    'filter': ['==', 'extrude', 'true'],
-    'type': 'fill-extrusion',
-    'minzoom': 15,
-    'paint': {
-    'fill-extrusion-color': '#aaa',
-     
-    // use an 'interpolate' expression to add a smooth transition effect to the
-    // buildings as the user zooms in
-    'fill-extrusion-height': ["get", "height"]
-    ,
-    'fill-extrusion-base':  ["get", "min_height"],
-    'fill-extrusion-opacity': .6
-    }
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+            'fill-extrusion-color': '#aaa',
+
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            'fill-extrusion-height': ["get", "height"],
+            'fill-extrusion-base': ["get", "min_height"],
+            'fill-extrusion-opacity': .6
+        }
     }, labelLayerId);
+
+    getTest().then(result =>  addLayers(map, result, "test"));
+
+});
+
+function addLayers(map, source, moduleName) {
+    map.addSource(moduleName, {
+        "type": "geojson",
+        "data": source
     });
-    
+    map.addLayer({
+        "id": moduleName + "-geom",
+        "type": "fill",
+        "source": moduleName,
+        "paint": {
+            "fill-color": "#66e100",
+            "fill-opacity": 0.5
+        }
+        /* ,
+                    "filter": ["==", "$type", "Polygon"] */
+    });
+    map.addLayer({
+        'id': moduleName + "-extrusion",
+        'type': 'fill-extrusion',
+        'source': moduleName,
+        'paint': {
+            // See the Mapbox Style Specification for details on data expressions.
+            // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions
+            // Get the fill-extrusion-color from the source 'color' property.
+            'fill-extrusion-color': '#66e100',
+            // Get fill-extrusion-height from the source 'height' property.
+            'fill-extrusion-height': 500,
+            // Get fill-extrusion-base from the source 'base_height' property.
+            'fill-extrusion-base': 5,
+            // Make extrusions slightly opaque for see through indoor walls.
+            'fill-extrusion-opacity': 0.7
+        }
+    });
+    console.log( 'done')
+}
 
-
-    /* map.addLayer({
-
-    }); */
+/* var mapBuilder = {
+    testSource: null,
+    test
+} */
     
 
 export default map;
